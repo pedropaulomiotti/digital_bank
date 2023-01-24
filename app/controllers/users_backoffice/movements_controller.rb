715c435fa33ac39398ed7d46743954fe133fe65b
Pedro Paulo dos Santos Miotti Fonseca <pedromiotti@gmail.com>
2023-01-23 23:49:39 -0300

    version 3.3

--- app/controllers/users_backoffice/movements_controller.rb
+++ app/controllers/users_backoffice/movements_controller.rb
@@ -2,27 +2,12 @@ class UsersBackoffice::MovementsController < UsersBackofficeController
   before_action :set_user
 
   def deposit
-    movement = Movement.new
-    movement.type = "deposit"
-    movement.value = params[:value].to_f
-    movement.user_id = current_user.id
-    movement.save
   end
 
   def withdraw
-    movement = Movement.new
-    movement.type = "withdraw"
-    movement.value = params[:value]
-    movement.user_id = current_user.id
-    movement.save
   end
 
   def transfer
-    movement = Movement.new
-    movement.type = "transfer"
-    movement.value = params[:value]
-    movement.user_id = current_user.id
-    movement.save
   end
 
   def movement
@@ -38,5 +23,13 @@ class UsersBackoffice::MovementsController < UsersBackofficeController
   def user_params
     params.permit(:full_name, :identification, :phone, :email, :birthday, 
                   :account_number, :account_balance, :password, :password_confirmation)
+  end
+
+  def set_movement
+    @movement = Movement.find(params[:id])
+  end
+
+  def movement_params
+    params.permit(:mode, :value, :user_id)
   end
 end
